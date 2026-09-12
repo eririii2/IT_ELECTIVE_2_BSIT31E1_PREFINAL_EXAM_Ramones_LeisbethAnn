871286308c5cff46a3ace63774992f1c7be71560
eririii2 <leisrylle@gmail.com>
2026-09-12 11:47:07 +0800

    save19

--- WebSystem-Prefinals_Exam/WebSystem-Prefinals_Exam/Controllers/ExamController.cs
+++ WebSystem-Prefinals_Exam/WebSystem-Prefinals_Exam/Controllers/ExamController.cs
@@ -240,6 +240,20 @@ namespace WebSystem_Prefinals_Exam.Controllers
                         "D. It automatically creates a ViewModel"
                     },
                     SelectedAnswer = "A"
+                },
+
+                new Question
+                {
+                    Id = 17,
+                    Text = "What is the purpose of a try...catch block in a controller?",
+                    Choices = new List<string>
+                    {
+                        "A. To create navigation properties",
+                        "B. To catch and handle exceptions that may occur during execution",
+                        "C. To generate database tables",
+                        "D. To perform client-side validation"
+                    },
+                    SelectedAnswer = "B"
                 }
             };
 
