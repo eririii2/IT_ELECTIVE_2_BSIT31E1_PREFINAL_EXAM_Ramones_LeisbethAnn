aee8e2e32eecf0f5ac0b1e3c33e67827709d8a99
eririii2 <leisrylle@gmail.com>
2026-09-12 08:12:54 +0800

    save1

---
+++ WebSystem-Prefinals_Exam/WebSystem-Prefinals_Exam/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebSystem_Prefinals_Exam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
