eaf421c369b25fa0882362d5e715ad073d1be9b9
eririii2 <leisrylle@gmail.com>
2026-09-12 11:20:21 +0800

    save11

--- WebSystem-Prefinals_Exam/WebSystem-Prefinals_Exam/Controllers/ExamController.cs
+++ WebSystem-Prefinals_Exam/WebSystem-Prefinals_Exam/Controllers/ExamController.cs
@@ -128,6 +128,20 @@ namespace WebSystem_Prefinals_Exam.Controllers
                         "D. Database connection string"
                     },
                     SelectedAnswer = "B"
+                },
+
+                new Question
+                {
+                    Id = 9,
+                    Text = "What is the purpose of a navigation property such as public Section Section { get; set; }?",
+                    Choices = new List<string>
+                    {
+                        "A. It stores the database password",
+                        "B. It represents a relationship to another entity",
+                        "C. It creates a new database",
+                        "D. It validates the student's name"
+                    },
+                    SelectedAnswer = "B"
                 }
             };
 
