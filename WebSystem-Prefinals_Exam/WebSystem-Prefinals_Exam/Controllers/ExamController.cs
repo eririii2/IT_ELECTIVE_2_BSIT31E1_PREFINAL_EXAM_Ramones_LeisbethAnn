b37a61cdbedde3ca4e7cfa90da4a793bab7ea3dd
eririii2 <leisrylle@gmail.com>
2026-09-12 11:43:47 +0800

    save18

--- WebSystem-Prefinals_Exam/WebSystem-Prefinals_Exam/Controllers/ExamController.cs
+++ WebSystem-Prefinals_Exam/WebSystem-Prefinals_Exam/Controllers/ExamController.cs
@@ -226,6 +226,20 @@ namespace WebSystem_Prefinals_Exam.Controllers
                         "D. Student Number should contain only spaces"
                     },
                     SelectedAnswer = "B"
+                },
+
+                new Question
+                {
+                    Id = 16,
+                    Text = "Which is the best reason for having a database-level unique constraint on StudentNumber?",
+                    Choices = new List<string>
+                    {
+                        "A. It protects data integrity even if application-level validation is bypassed",
+                        "B. It makes Razor Views render faster",
+                        "C. It removes the need for a Controller",
+                        "D. It automatically creates a ViewModel"
+                    },
+                    SelectedAnswer = "A"
                 }
             };
 
